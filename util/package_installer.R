7e5fac64e7a644a8ae42b5004fda6bd35ba3edf2
cramirez98 <camilo.706@hotmail.com>
2022-03-16 18:22:32 -0500

    Se modifico package_installer para mostrar el numero de paquetes a instalar

--- util/package_installer.R
+++ util/package_installer.R
@@ -5,7 +5,7 @@ library(tictoc)
 
 package_list <- read.csv("paquetes.csv", header= T)$PAQUETES
 missin_packages <- package_list[!(package_list %in% installed.packages()[,"Package"])]
-print('Paquetes a instalar:')
+print(paste('Numero de paquetes a instalar:', length(missin_packages)))
 print(missin_packages)
 tic('Tiempo de instalacion de paquetes:')
 if(length(missin_packages)) install.packages(missin_packages)
