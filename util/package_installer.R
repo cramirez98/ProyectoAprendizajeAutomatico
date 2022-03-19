12746a4887014776a560188e817670798c6079cd
cramirez98 <camilo.706@hotmail.com>
2022-03-19 10:43:38 -0500

    Se actualizo package_installer.R

--- util/package_installer.R
+++ util/package_installer.R
@@ -1,9 +1,14 @@
 #!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
 
 install.packages('tictoc')
 library(tictoc)
 
-package_list <- read.csv("paquetes.csv", header= T)$PAQUETES
+if(length(args) == 0){
+  stop("No se suministro argumentos")
+}else{
+  package_list <- read.csv("paquetes.csv", header= T)[,toupper(args[1])]
+}
 missin_packages <- package_list[!(package_list %in% installed.packages()[,"Package"])]
 print(paste('Numero de paquetes a instalar:', length(missin_packages)))
 print(missin_packages)
